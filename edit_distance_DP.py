0a50e1bfd41f58b1659fe811934cfa05aae89fb5
Joanna Guo <guo.joanna@hotmail.com>
2019-12-07 20:11:51 -0500

    clean up, added comments

--- edit_distance_DP.py
+++ edit_distance_DP.py
@@ -2,17 +2,30 @@ import sys
 from numpy import zeros
 
 def edDistDp(x, y):
-    """ Calculate edit distance between sequences x and y using
-        matrix dynamic programming.  Return distance. """
-    D = zeros((len(x)+1, len(y)+1), dtype=int)
-    D[0, 1:] = range(1, len(y)+1)
-    D[1:, 0] = range(1, len(x)+1)
-    for i in range(1, len(x)+1):
-        for j in range(1, len(y)+1):
-            delt = 1 if x[i-1] != y[j-1] else 0
-            D[i, j] = min(D[i-1, j-1]+delt, D[i-1, j]+1, D[i, j-1]+1)
-    return D[len(x), len(y)]
+	"""
+	Calculate edit distance between sequences x and y using matrix dynamic programming.
+
+	Parameters
+	----------
+	x : string sequence 1
+	y : string sequence 2
+
+	Returns
+	-------
+	d : edit distance
+
+	"""
+	D = zeros((len(x)+1, len(y)+1), dtype=int)
+	D[0, 1:] = range(1, len(y)+1)
+	D[1:, 0] = range(1, len(x)+1)
+
+	# fill in DP matrix row by row
+	for i in range(1, len(x)+1):
+		for j in range(1, len(y)+1):
+			delt = 1 if x[i-1] != y[j-1] else 0
+			D[i, j] = min(D[i-1, j-1]+delt, D[i-1, j]+1, D[i, j-1]+1)
+	return D[len(x), len(y)]
 
 if __name__=="__main__":
-    true_file, edit_file = int(sys.argv[1]), int(sys.argv[2])
-    
+	true_file, edit_file = int(sys.argv[1]), int(sys.argv[2])
+	
